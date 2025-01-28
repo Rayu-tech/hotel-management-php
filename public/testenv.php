6313bd5d14bf227b3813759e530e7fa2469b104d
kena2018 <kena.patel@techifysolutions.com>
2025-01-28 18:57:58 +0530

    Initial commit

---
+++ public/testenv.php
@@ -0,0 +1,7 @@
+<?php
+// Display the values of the environment variables
+echo "DB Name: " . getenv("env_db_name") . "<br>";
+echo "DB User: " . getenv("env_user_name") . "<br>";
+echo "DB Password: " . getenv("env_password") . "<br>";
+echo "DB Host: " . getenv("env_db_host") . "<br>";
+?>
